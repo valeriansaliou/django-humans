a5b1e7d610885382ac72e620bc567978a807f71c
Valérian Saliou <valerian@valeriansaliou.name>
2013-10-15 21:38:32 +0200

    Bump up to v0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='django-humans',
-    version='0.1',
+    version='0.2',
     author=u'Jason Novinger',
     author_email='jnovinger@gmail.com',
     packages=find_packages(),
