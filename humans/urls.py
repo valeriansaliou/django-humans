1270a32ddfa5ac51863fbfc7e503ca7e180406b6
Valérian Saliou <valerian@valeriansaliou.name>
2013-10-17 07:05:06 +0200

    Up to 0.2alt

--- humans/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import *
-
-from humans import views
-
-urlpatterns = patterns('',
-    url(r'$', views.humans, name='humans'),
-)
--- humans/views.py
+++ humans/views.py
@@ -3,12 +3,12 @@ from django.template import RequestContext
 
 from humans.models import HumanGroup, Snippet
 
-def humans(request):
+def humans_txt(request, template='humans.txt'):
     snippet = Snippet.objects.random_snippet()
     groups = HumanGroup.objects.exclude(members__isnull=True)
 
     return render_to_response(
-        template_name='humans/humans.txt',
+        template_name=template,
         dictionary={
             'snippet': snippet,
             'groups': groups,
